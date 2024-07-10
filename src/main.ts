3d5b5dc289de91c347109d92b7695aef256444d4
VictorTimbo <manoelvictortimbo@gmail.com>
2024-07-10 18:59:26 -0300

    create branch develop

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import './assets/main.css'
+import '@/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button';
 import router from './router'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { RiHeartFill, RiHeartAddLine, HiMenu } from "oh-vue-icons/icons";
+import { setComponent } from './modules/component.module';
 
-const app = createApp(App)
+addIcons(RiHeartFill, RiHeartAddLine, HiMenu);
 
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+setComponent(app);
+app.use(PrimeVue, {
+    unstyled: true
+})
+app.use(router);
+app.component("v-icon", OhVueIcon);
+app.component("Button", Button);
+app.mount('#app');
