0c34cdfe985f8bdd2692e8b444c436c58b42a3c9
VictorTimbo <manoelvictortimbo@gmail.com>
2024-07-21 21:44:20 -0300

    21:44 21/07/24

--- src/views/Favorite/favorite.vue
+++ src/views/Favorite/favorite.vue
@@ -1,19 +1,24 @@
 <script lang="ts">
-import { FavoritesManager } from '@/views/Favorite/favorite.service';
+import type { StreamingsContents } from '@/models/streaming.model';
+import { FavoritesService } from '@/views/Favorite/favorite.service';
 
 export default {
     data() {
         return {
-            favorites: new FavoritesManager()
+
+        }
+    },
+    computed: {
+        serviceFavorites(): FavoritesService {
+            return new FavoritesService();
         }
     },
     methods: {
-        removeFavorite(value: Object) {
-            this.favorites.removeFavorite(value)
+        removeFavorite(value: StreamingsContents) {
+            this.serviceFavorites.removeFavorite(value)
         },
         clearAll() {
-            this.favorites.clearFavorites()
-            localStorage.clear()
+            this.serviceFavorites.clearFavorites()
         }
     }
 }
@@ -23,12 +28,12 @@ export default {
     <div class="bg-gray-950 text-center">
         <div>
             <Button type="button" @click="clearAll"
-                class=" rounded-md bg-white m-6 p-1 text-black text-md font-semibold outline-none hover:ring-2 hover:ring-neutral-300 hover:ring-offset-2 hover:ring-offset-gray-800  transition duration-500 hover:scale-105">
+                class=" rounded-md bg-white m-6 p-1 text-black text-md font-semibold outline-none hover:ring-2 hover:ring-neutral-300 hover:ring-offset-2 hover:ring-offset-gray-800  transition duration-300 hover:scale-105">
                 Limpar
             </Button>
         </div>
         <div>
-            <background :url="favorites.favoritesList.list" :botao="true"/> 
+            <background :url="serviceFavorites.favoritesList.list" :botao="true"/> 
         </div>  
     </div>
 </template>
--- src/views/details.vue
+++ src/views/details.vue
@@ -1,26 +1,35 @@
 <script lang="ts">
 import { StreamingsContents } from '@/models/streaming.model';
 import { StreamingService } from '@/views/streaming.service';
-import { FavoritesManager } from '@/views/Favorite/favorite.service';
+import { FavoritesService } from '@/views/Favorite/favorite.service';
 
 export default {
     data() {
         return {
             detail: new StreamingsContents(),
-            favorites: new FavoritesManager(),
-            btn: true,
+            newFavorite: {
+                media: this.$route.params.media.toString(),
+                id: Number(this.$route.params.id)
+            },
+            isFavorite: false,
         };
     },
     created() {
         this.getDetails();
+        this.checkFavoriteStatus();
     },
     computed: {
         service(): StreamingService {
             return new StreamingService();
         },
+        serviceFavorites(): FavoritesService {
+            return new FavoritesService();
+        },
         firstTrailer() {
             if (this.detail.videos && this.detail.videos.results) {
-                return this.detail.videos.results.find(video => video.type === 'Trailer' || video.type === 'Teaser');
+                return this.detail.videos.results.find(
+                    video => video.type === 'Trailer' || video.type === 'Teaser'
+                );
             }
             return null;
         }
@@ -36,49 +45,40 @@ export default {
                 })
             this.service.getDetailStreaming(Number(this.$route.params.id.toString()), this.$route.params.media.toString()!)
         },
-        submit() {
-            this.btn = !this.btn;
-            /*this.favorites.favoritesList.newFavorite = {
-                media:  this.$route.params.media.toString(),
-                id: Number(this.$route.params.id)
-            }*/
-            if (this.detail && !this.favorites.favoritesList.list.includes(this.detail)) {
-                this.favorites.addFavorite(this.detail);
+        toggleFavorite() {
+            if (this.isFavorite) {
+                this.serviceFavorites.removeFavorite(this.newFavorite);
+            } else {
+                this.serviceFavorites.addFavorite(this.newFavorite);
             }
+            this.isFavorite = !this.isFavorite;
         },
-        remove(favorite: StreamingsContents) {
-            this.favorites.removeFavorite(favorite)
-            this.btn = !this.btn;
-        },
+        checkFavoriteStatus() {
+            this.isFavorite = this.serviceFavorites.isFavorite(this.newFavorite);
+        }
     }
 }
 </script>
 
 <template>
     <div class="relative">
-        <img v-if="detail.backdrop_path" class="w-full h-screen object-cover" :src="`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`"/>
-        <img v-else-if="detail.poster_path" class="w-full h-screen object-cover" :src="`https://image.tmdb.org/t/p/original/${detail.poster_path}`"/>
-        <img v-else class="w-full h-screen object-cover" :src="`https://image.tmdb.org/t/p/original/${detail.profile_path}`"/>
+        <img v-if="detail.backdrop_path" class="w-full h-screen object-cover"
+            :src="`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`" />
+        <img v-else-if="detail.poster_path" class="w-full h-screen object-cover"
+            :src="`https://image.tmdb.org/t/p/original/${detail.poster_path}`" />
+        <img v-else class="w-full h-screen object-cover"
+            :src="`https://image.tmdb.org/t/p/original/${detail.profile_path}`" />
         <div
             class="absolute flex flex-col justify-center items-center lg:flex-row lg:justify-around lg:items-center w-full h-screen top-0 left-0 bg-gradient-to-r from-black from-50% to-transparent opacity-95 p-8 lg:p-16">
             <div
                 class="text-white max-w-md text-center mt-4 mb-4 lg:mt-0 lg:w-1/2 lg:pr-8 lg:flex lg:flex-col lg:justify-center lg:ml-16">
-                <div class="text-3xl lg:text-5xl font-bold">
-                    {{ detail.title || detail.name }}
-                </div>
-                <div class="mt-2 lg:mt-4 w-full text-justify text-xl tracking-wide">
-                    {{ detail.overview }}
-                </div>
+                <div class="text-3xl lg:text-5xl font-bold">{{ detail.title || detail.name }}</div>
+                <div class="mt-2 lg:mt-4 w-full text-justify text-xl tracking-wide">{{ detail.overview }}</div>
                 <div class="mt-4 lg:mt-8">
-                    <Button v-if="!btn" @click="submit"
-                        class="bg-red-700 rounded text-black font-semibold hover:bg-black hover:text-red-700 focus:outline-none focus:text-red-700 focus:bg-black focus:ring-2 focus:ring-white hover:ring-2 hover:ring-white p-1">
-                        Favoritar
-                        <v-icon name="ri-heart-add-line"></v-icon>
-                    </Button>
-                    <Button v-else @click="remove(detail)"
-                        class="bg-red-700 rounded-md text-black font-semibold hover:bg-black hover:text-red-700 outline-none focus:text-red-500 focus:bg-black hover:ring-2 hover:ring-white p-1">
-                        Remover
-                        <v-icon name="fa-heart-broken"></v-icon>
+                    <Button @click="toggleFavorite"
+                        class="rounded bg-red-700 text-black font-semibold hover:bg-black hover:text-red-700 hover:outline-none hover:ring-2 hover:ring-white p-1">
+                        {{ isFavorite ? 'Remover' : 'Favoritar' }}
+                        <v-icon :name="isFavorite ? 'fa-heart-broken' : 'ri-heart-add-line'"></v-icon>
                     </Button>
                 </div>
             </div>
@@ -87,13 +87,10 @@ export default {
                 <iframe class="w-full h-full" :src="`https://www.youtube.com/embed/${firstTrailer.key}`"
                     :title="`${detail.name || detail.title}`" frameborder="0" allowfullscreen></iframe>
             </div>
-            <div v-else-if="detail.backdrop_path"
-                class="w-4/5 h-60 sm:h-72 sm:w-2/3 md:h-96 md:w-2/3 lg:w-[700px] lg:h-[430px] overflow-hidden lg:mb-0 lg:ml-8 lg:flex lg:items-center">
-                <img class="w-full h-full" :src="`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`" />
-            </div>
             <div v-else
                 class="w-4/5 h-60 sm:h-72 sm:w-2/3 md:h-96 md:w-2/3 lg:w-[700px] lg:h-[430px] overflow-hidden lg:mb-0 lg:ml-8 lg:flex lg:items-center">
-                <img class="w-full h-full" :src="`https://image.tmdb.org/t/p/original/${detail.profile_path}`" />
+                <img class="w-full h-full"
+                    :src="`https://image.tmdb.org/t/p/original/${detail.backdrop_path || detail.profile_path}`" />
             </div>
         </div>
     </div>
